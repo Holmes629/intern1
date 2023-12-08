ec547b6d014e2093b29ddf055a40c4204996a7bf
Holmes629 <98832423+Holmes629@users.noreply.github.com>
2023-12-08 23:55:43 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,35 @@
+<?php
+// Database connection settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     // Escape user inputs to prevent SQL injection
+//     $name = mysqli_real_escape_string($conn, $_POST['name']);
+//     $email = mysqli_real_escape_string($conn, $_POST['email']);
+//     $subject = mysqli_real_escape_string($conn, $_POST['subject']);
+//     $message = mysqli_real_escape_string($conn, $_POST['message']);
+
+//     // Insert user data into database
+//     $sql = "INSERT INTO test_database (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+//     if ($conn->query($sql) === TRUE) {
+//         echo "New record created successfully";
+//     } else {
+//         echo "Error: " . $sql . "<br>" . $conn->error;
+//     }
+// }
+
+$conn->close();
+?>
+
