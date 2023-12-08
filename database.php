12db6e90b213b559a1673c3fd3142b7ddd82919a
Holmes629 <98832423+Holmes629@users.noreply.github.com>
2023-12-09 00:00:12 +0530

    Update database.php

--- database.php
+++ database.php
@@ -13,22 +13,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     // Escape user inputs to prevent SQL injection
-//     $name = mysqli_real_escape_string($conn, $_POST['name']);
-//     $email = mysqli_real_escape_string($conn, $_POST['email']);
-//     $subject = mysqli_real_escape_string($conn, $_POST['subject']);
-//     $message = mysqli_real_escape_string($conn, $_POST['message']);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Escape user inputs to prevent SQL injection
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $subject = mysqli_real_escape_string($conn, $_POST['subject']);
+    $message = mysqli_real_escape_string($conn, $_POST['message']);
 
-//     // Insert user data into database
-//     $sql = "INSERT INTO test_database (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+    // Insert user data into database
+    $sql = "INSERT INTO test_database (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
 
-//     if ($conn->query($sql) === TRUE) {
-//         echo "New record created successfully";
-//     } else {
-//         echo "Error: " . $sql . "<br>" . $conn->error;
-//     }
-// }
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
 
 $conn->close();
 ?>
